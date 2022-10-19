f8fc6c4e98c0dc4db89aa7a3d055d4fe6ec9313b
chriselliott97 <chris.elliott6497@gmail.com>
2022-10-19 18:35:53 -0400

    pts 2 #5 done

--- src/Components/SuperMarket/SuperMarket.jsx
+++ src/Components/SuperMarket/SuperMarket.jsx
@@ -1,19 +1,27 @@
 import '../../styles/super-market.css'
+import { useState } from 'react'
 
 // Components & Data
 import { products } from '../../data/market-data'
+import MarketNav from './MarketNav'
+import DisplayProducts from './DisplayProducts'
+import Cart from './Cart'
 
-const SuperMarket = () => {
+const SuperMarket = (props) => {
   console.log('Imported product data:::', products)
+
+  const [cart, setCart] = useState([])
+  const [productCategory, setProductCategory] = useState('Produce')
+
   return (
     <div className="super-market">
       <section>
-        MarketNav component here
-        DisplayProducts component here
+        <MarketNav products={products} setProductCategory={setProductCategory}/>
+        <DisplayProducts products={products} productCategory={productCategory}/>
       </section>
 
-      Cart component here
 
+      <Cart />
     </div>
   )
 }
